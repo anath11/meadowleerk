b03bd71d933f54e520301fc684ffd9563e516cd8
Ashok <ashoknath@Ashoks-MBP.hub>
2021-08-05 23:32:49 +1000

    Adding testing file to the project

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const expressHandlebars = require('express-handlebars');
-const fortune = require('./lib/fortune');
+
+//Internal library
+const handlers = require('./lib/handlers');
 
 // Config handlebar templating engine to the web app
 app.engine('handlebars', expressHandlebars({
@@ -9,42 +11,24 @@ app.engine('handlebars', expressHandlebars({
 }));
 app.set('view engine','handlebars');
 
-//Adding route with get method 
-
-// Home Page route
-app.get('/', (req,res)=>{res.render('home')});
-
-//About page route
-app.get('/about', (req,res)=>{
-    // const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];    
-    res.render('about', {fortunes:fortune.getFortune()});
-})
-
 // Adding public directory for serving stati media such as html, css and image
 app.use(express.static(__dirname + '/public'));
 
+//Adding route with get method 
+
+// Home Page route
+app.get('/', handlers.home);
+
+//About page route
+app.get('/about',handlers.about)
+
+
 // Custome 404 not found page
-app.use( (req,res)=>{
-    res.status(404);
-    res.render('404');
-} );
+app.use(handlers.notFound );
 
 // Custom 500 Internal server error
-app.use( (err,req,res, next) => {
-    console.log(err.message);
-    res.status(500);
-    res.render('500');
-} );
+app.use( handlers.serverError);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{console.log(`Listening to port ${PORT}....`)});   
-
-// // Extra js functon which will be removed leater of
-// const fortunes = [
-//     "Conquer your fears or they will conquer you",
-//     "River never springs",
-//     "Do not fear what you don't know",
-//     "You will have a pleasent suprise",
-//     "Whenever possible, keep it simple"
-// ];
