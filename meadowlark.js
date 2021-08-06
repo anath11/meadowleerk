7bbf6e8a4a620b185b41aa9c20e0fea112a80d84
Ashok <ashoknath@Ashoks-MacBook-Pro.local>
2021-08-06 15:41:36 +1000

    Adding esling for clear code

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,4 @@
-const e = require('express');
+// const e = require('express');
 const express = require('express');
 const app = express();
 const expressHandlebars = require('express-handlebars');
@@ -32,10 +32,9 @@ app.use( handlers.serverError);
 
 const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, ()=>{console.log(`Listening to port ${PORT}....`)});   
-
 if(require.main === module){
     app.listen(PORT, ()=>{console.log(`Listening to port ${PORT}....`)});   
 } else {
     module.exports = app;
 }
+ 
